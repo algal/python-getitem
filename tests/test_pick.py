83428383efa10ff3a0f2e46db32fb265e60ae5d2
Alexis Gallagher <alexis@alexisgallagher.com>
2024-11-25 13:47:04 -0800

    add test file

---
+++ tests/test_pick.py
@@ -0,0 +1,18 @@
+import pytest, os, tempfile, textwrap, tomlkit  # noqa: E401
+
+
+
+s1 = """
+On branch dev-longcontexteval
+Your branch is up to date with 'origin/dev-longcontexteval'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	bert24-base-v2.yaml
+	r_first50000.json
+	src/evals/items2000.json
+	src/evals/rewritten10.json
+
+nothing added to commit but untracked files present (use "git add" to track)
+"""
+
