18a81050c83a703e706b493bcb102ea7df971994
Alexis Gallagher <alexis@alexisgallagher.com>
2024-11-25 22:50:02 -0800

    refactor. untested.

--- src/pick.py
+++ src/pick.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python3
 
-import sys
-import re
+import sys, re, argparse
 from collections import deque
+from collections.abc import Iterable
 
-def usage():
-    print("Usage: {} [--onebased | -o | -u] ROW COLUMN".format(sys.argv[0]))
-    print("")
-    print("Filter stdin and print specific rows and columns, specifying")
-    print("them in Python's slicing syntax.")
-    print("Optionally, can use one-based indexes, unlike Python.")
-    print("")
-    print("For example:")
-    print("cat myfile | ./pick :5 0     # Print the column 0 of the first 5 rows.")
-    print("cat myfile | ./pick 0 :      # Print the first row, all of it.")
-    print("cat myfile | ./pick -o 1 :   # Print the first row, all of it.")
-    print("cat myfile | ./pick -10 0:2  # Print the first 2 columns of the last 10 rows.")
-    print("cat myfile | ./pick -2:-1 : # Prints all fields of the second to last row.")
-    print("")
-    sys.exit(1)
-
-def parse_input(value,should_decr_pos=False):
+def parse_input(value:str,should_decr_pos:bool=False) -> int | slice:
     "parses value into an int, or an int:Optional[int] slice"
     retval = None
     if ":" in value:
@@ -32,8 +16,8 @@ def parse_input(value,should_decr_pos=False):
     if not should_decr_pos:
       return retval
     else:
-      def decr_pos(i): return (i-1) if (isinstance(i, int) and i > 0) else i
-      
+      def decr_pos(i):
+          return (i-1) if (isinstance(i, int) and i > 0) else i
       if isinstance(retval, int):
         return decr_pos(retval)
       elif isinstance(retval, slice):
@@ -45,72 +29,78 @@ def parse_input(value,should_decr_pos=False):
 def split_with_positions(input_str, pattern):
     return [(match.group(), match.start(), match.end()) for match in re.finditer(pattern, input_str)]
 
-def print_filtered_line(line, col):
+def filtered_line(line:str, col: int | slice) -> str | None:
     "col : a slice or int"
     line = line.rstrip()
     fields = split_with_positions(line, r'\S+|\s+')
     non_space_fields = [field for field in fields if not field[0].isspace()]
     if not non_space_fields:
-        print()
-        return
+        return None
     if isinstance(col, slice):
         start_pos = non_space_fields[col.start][1] if col.start is not None else 0
         end_pos = non_space_fields[col.stop-1][2] if col.stop is not None else len(line)
-        print(line[start_pos:end_pos])
+        return line[start_pos:end_pos]
     else:
-        print(non_space_fields[col][0])
+        return non_space_fields[col][0]
 
-def apply_filters(i, row, col, line):
-    "Filters and prints fields of the ith row. i>=0."
-    if isinstance(row, slice):
-        start, stop = row.start if row.start else 0, row.stop if row.stop else float('inf')
-        if start <= i < stop:
-            # Bug: this join transforms all whitespace separations into spaces
-            print_filtered_line(line,col)
-    elif i == row:
-        print_filtered_line(line,col)
+def pick(lines:Iterable[str],row_spec:str,col_spec:str) -> Iterable[str]:
+    row = parse_input(row_spec)
+    column = parse_input(col_spec)
+    out_lines = super_islice(lines,row_spec)
+    for line in out_lines:
+        out = filtered_line(line,column)
+        if out: yield out
 
-def pick(s:str):
-    return ""
+def super_islice(iterable,slice_spec:str) -> Iterable:
+    "slice_spec, a str of an index or a splice, like '-5:' etc.."
+    if ":" in slice_spec:
+        beg,end = tuple(slice_spec.split(":"))
+        beg = 0 if beg=='' else int(beg)
+        end = float('inf') if end=='' else int(end)
+    else:
+        beg = int(slice_spec)
+        end = beg + 1 if beg!=-1 else float('inf')
+    # assert: (beg,end):(+int,+int)
+    buf = None
+    if beg < 0:
+        buf = deque(maxlen=abs(beg))
+    elif end < 0:
+        buf = deque()
+    if buf is None:
+        src = enumerate(iterable)
+    else:
+        lastrow = 0
+        for i,line in enumerate(iterable):
+            buf.append((i,line))
+            lastrow = i
+        (beg,end) = tuple([x if x>=0 else x+1+lastrow for x in (beg,end)])
+        src = buf
+    for i,line in src:
+        if beg <= i and i < end:
+            yield line
 
 def main():
-    onebased = False
-    if "--onebased" in sys.argv:
-        onebased = True
-        sys.argv.remove("--onebased")
-    if "-o" in sys.argv:
-        onebased = True
-        sys.argv.remove("-o")
-    if "-u" in sys.argv:
-        onebased = True
-        sys.argv.remove("-u")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("row",    type=str, help="Row specification")
+    parser.add_argument("column", type=str, help="Column specification")
+    parser.prefix_chars=''
+    parser.epilog="""
+    Filter stdin and print specific rows and columns, specifying
+    them in Python's slicing syntax.
+    
+    For example:
+    cat myfile | ./pick :5 0     # Print the column 0 of the first 5 rows.
+    cat myfile | ./pick 0 :      # Print the first row, all of it.
+    cat myfile | ./pick -o 1 :   # Print the first row, all of it.
+    cat myfile | ./pick -10 0:2  # Print the first 2 columns of the last 10 rows.
+    cat myfile | ./pick -2:-1 : # Prints all fields of the second to last row.
+    """
+    args = parser.parse_args()
 
-    if len(sys.argv) != 3:
-        usage()
-
-    row = parse_input(sys.argv[1],onebased)
-    column = parse_input(sys.argv[2],onebased)
-
-    buffer = None
-    if isinstance(row, int) and row < 0:
-        buffer = deque(maxlen=abs(row))
-    elif isinstance(row,slice) and row.start < 0:
-        buffer = deque(maxlen=abs(row.start))
-
-    for i, line in enumerate(sys.stdin):
-        if buffer is not None:
-            buffer.append((i, line))
-        else:
-            apply_filters(i, row, column, line)
-
-    if buffer is not None:
-        lastrow = buffer[-1][0]
-        if isinstance(row,int):
-            row = lastrow + 1 + row
-        else:
-            row = slice(lastrow + 1 + row.start, row.stop)
-        for i, line in buffer:
-            apply_filters(i, row, column, line)
+    in_lines = (line for line in sys.stdin)
+    out_lines = pick(in_lines, args.row, args.column)
+    for line in out_lines:
+        print(line)
 
 if __name__ == "__main__":
     main()
--- tests/test_pick.py
+++ tests/test_pick.py
@@ -5,7 +5,7 @@ from pick import pick
 
 
 
-s1:tuple[str,str] = ("""
+s1:tuple[str,tuple[str,str],str] = ("""
 On branch dev-longcontexteval
 Your branch is up to date with 'origin/dev-longcontexteval'.
 
@@ -18,13 +18,46 @@ Untracked files:
 
 nothing added to commit but untracked files present (use "git add" to track)
 """,
+("5:-2",":"),
 """
 	bert24-base-v2.yaml
 	r_first50000.json
 	src/evals/items2000.json
 	src/evals/rewritten10.json
- """)
+""")
 
-def test_run():
-    assert s1[1] == pick(s1[0])
+s2 = ("""
+AAA
+BBB
+CCC
+""",
+("1",":"),
+"""
+BBB
+"""
+)
 
+def f(s): return textwrap.dedent(s).lstrip()
+def g(t): return (f(t[0]),t[1],f(t[2]))
+
+tests:list[tuple] = [g(x) for x in [s1,s2]]
+
+@pytest.mark.parametrize("pidx", list(range(len(tests))))
+def test_run(pidx:int):
+    s = tests[pidx]
+    lines = (line for line in s[0].strip().splitlines())
+    actual = list(pick(lines,s[1][0],s[1][1]))
+    expected = list(s[2].strip().splitlines())
+    assert actual == expected
+
+def test_parse_slices():
+    from pick import parse_input
+    assert parse_input("5") == 5
+    assert parse_input("0") == 0
+    assert parse_input("-5") == -5
+    assert parse_input("1:1") == slice(1,1)
+    assert parse_input("5:") == slice(5,None)
+    assert parse_input("-5:-2") == slice(-5,-2)
+    assert parse_input("-5:") == slice(-5,None)
+    assert parse_input(":-5") == slice(0,-5)
+    
