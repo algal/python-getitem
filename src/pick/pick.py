c2cc5982ec6dba6700411c6f58ee2c1587ff4d9c
Alexis Gallagher <alexis@alexisgallagher.com>
2024-11-27 18:34:29 -0800

    refactor

--- src/pick/pick.py
+++ src/pick/pick.py
@@ -14,7 +14,7 @@ def parse_slice_spec(slice_spec:str,should_decr_pos:bool=False) -> slice:
         end = beg + 1 if beg!=-1 else float('inf')
     return slice(beg,end)
 
-def split_with_positions2(str_to_split, pattern):
+def split_with_positions(str_to_split, pattern):
     "Returns (s,beg,end) for every substring in between the split"
     result = []
     last_end = 0
@@ -29,7 +29,7 @@ def split_with_positions2(str_to_split, pattern):
 def filtered_line(line:str, col: slice) -> str | None:
     "col : a slice or int"
     line = line.rstrip()
-    fields = split_with_positions2(line, r'\s+')
+    fields = split_with_positions(line, r'\s+')
     if not fields:
         return None
     start_pos = fields[col.start][1]
@@ -75,36 +75,12 @@ def super_islice(iterable,slice_spec:str) -> Iterable:
         if beg <= i and i < end:
             yield line
 
-def mainOLD():
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-h","--help", action="help")
-    parser.add_argument("-f","--file", help="Input file path")
-    parser.add_argument("row",    type=str, help="Row specification")
-    parser.add_argument("column", type=str, help="Column specification")
-#    parser.prefix_chars=''
-    parser.epilog="""
-    Filter stdin and print specific rows and columns, specifying
-    them in Python's slicing syntax.
-    
-    For example:
-    cat myfile | ./pick :5 0     # Print the column 0 of the first 5 rows.
-    cat myfile | ./pick 0 :      # Print the first row, all of it.
-    cat myfile | ./pick -10 0:2  # Print the first 2 columns of the last 10 rows.
-    cat myfile | ./pick -2:-1 : # Prints all fields of the second to last row.
-    """
-    args = parser.parse_args()
-
-    in_lines = (line for line in sys.stdin)
-    out_lines = pick(in_lines, args.row, args.column)
-    for line in out_lines:
-        print(line)
-
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
 #    usage='%(prog)s [-h] [-f FILE] row_slice column_slice'
     usage='%(prog)s [-h] row_slice column_slice'
-                                     )
 #    parser.add_argument("-f","--file", help="Input file path")
+    # convoluted parsing needed to allow positional arguments which start with HYPHEN-MINUS
     parser.add_argument("row_col_specs", nargs=argparse.REMAINDER, help="Row and column specifications")
     parser.epilog="""
     Filter stdin and print specific rows and columns, specifying
