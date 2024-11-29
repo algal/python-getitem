571f77c20656b786b316bc4bb2ea304660668161
Alexis Gallagher <alexis@alexisgallagher.com>
2024-11-28 22:49:00 -0800

    refactor

--- src/pick/__init__.py
+++ src/pick/__init__.py
@@ -1,8 +1,8 @@
-from .pick import main, pick
+from .pick import main, pick, islice
 # from the pick.py module, import the functions main and pick
 
 # export the functions main and pick
-__all__ = ["main","pick"]
+__all__ = ["main","pick","islice",]
 
 # for debugging from notebooks
 #from .pick import *
--- src/pick/pick.py
+++ src/pick/pick.py
@@ -3,6 +3,17 @@ import sys, re, getopt, pathlib
 from collections import deque
 from collections.abc import Iterable
 
+def slice_from_spec(slice_spec:str) -> slice:
+    "parses value into a slice"
+    if ":" in slice_spec:
+        beg,end = tuple(slice_spec.split(":"))
+        beg = 0 if beg=='' else int(beg)
+        end = None if end=='' else int(end)
+    else:
+        beg = int(slice_spec)
+        end = beg + 1 if beg!=-1 else None
+    return slice(beg,end)
+
 def parse_slice_spec(slice_spec:str,should_decr_pos:bool=False) -> tuple:
     "parses value into an (int,int)"
     if ":" in slice_spec:
@@ -26,25 +37,27 @@ def split_with_positions(str_to_split, pattern):
         result.append((str_to_split[last_end:], last_end, len(str_to_split)))
     return result
 
-def filtered_line(line:str, col_spec: str) -> str | None:
+def filtered_line(line:str, col_slice: slice) -> str | None:
     "col : a slice or int"
     eol_to_preserve = "\n" if line.endswith("\n") else ""
     fields = split_with_positions(line, r'\s+')
-    sliced_fields = list(islice(fields,col_spec))
+    sliced_fields = fields[col_slice]
     if not sliced_fields: return None
-    col = parse_slice_spec(col_spec)
     start_pos = sliced_fields[  0 ][1]
     end_pos   = sliced_fields[ -1 ][2]
+    # TODO: fill out tabs properly?
     fl = (" " * start_pos) + line[start_pos:end_pos] + eol_to_preserve
     return fl
 
 def pick(lines:Iterable[str],row_spec:str,col_spec:str,line_count=None) -> Iterable[str]:
-    out_lines = islice(lines,row_spec,iterable_len=line_count)
+    out_lines = islice(lines,
+                        slice_from_spec(row_spec),
+                        iterable_len=line_count)
     for line in out_lines:
-        out = filtered_line(line,col_spec)
+        out = filtered_line(line,slice_from_spec(col_spec))
         if out: yield out
 
-def islice2(iterable,sl:slice,iterable_len=None) -> Iterable:
+def islice(iterable,sl:slice,iterable_len=None) -> Iterable:
     "Consumes iterable once. Maintains a buffer if necessary."
     def normalize(beg:int|None,end:int|None,length=None) -> tuple[int,int|None]:
         begi:int = 0 if beg is None else beg
@@ -68,36 +81,6 @@ def islice2(iterable,sl:slice,iterable_len=None) -> Iterable:
         src = buf
     for i,line in src:
         if beg <= i and (end is None or i < end):
-            yield line
-
-def islice(iterable,slice_spec:str,iterable_len=None) -> Iterable:
-    """
-    slice_spec, a str of an index or a splice, like '-5:' etc..
-
-    Consumes iterable once. Maintains a buffer if necessary.
-    """
-    if hasattr(iterable,'__len__'): iterable_len = len(iterable)
-    (beg,end) = parse_slice_spec(slice_spec)
-    if iterable_len is not None:
-        beg = (beg + iterable_len) if (beg<0) else beg
-        end = (end + iterable_len) if (end<0) else end
-    if beg < 0:
-        buf = deque(maxlen=abs(beg))
-    elif end < 0:
-        buf = deque()
-    else:
-        buf = None
-    if buf is None:
-        src = enumerate(iterable)
-    else:
-        lastrow = 0
-        for i,line in enumerate(iterable):
-            buf.append((i,line))
-            lastrow = i
-        (beg,end) = tuple([x if x>=0 else x+1+lastrow for x in (beg,end)])
-        src = buf
-    for i,line in src:
-        if beg <= i and i < end:
             yield line
 
 def usage():
